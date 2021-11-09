6c17fe0168cb43c781e55dde532ee3be91dab90c
Marina <mnovikova@elinext.com>
2021-11-09 15:50:28 +0300

    add config

--- wdio.conf.js
+++ wdio.conf.js
@@ -101,7 +101,7 @@ specs: ["./test/specs/**/*.js"],
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
-  connectionRetryTimeout: 120000,
+  connectionRetryTimeout: 60000,
   //
   // Default request retries count
   connectionRetryCount: 3,
