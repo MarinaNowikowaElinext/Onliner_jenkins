8ba6ca746c26b8069df0ab5776140e7c6519ee4c
Marina <mnovikova@elinext.com>
2021-11-09 16:22:58 +0300

    fix bugs

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-specs: ["./test/specs/**/dollar-rate.js"],
+specs: ["./test/specs/**/*.js"],
   
   // Patterns to exclude.
   exclude: [
